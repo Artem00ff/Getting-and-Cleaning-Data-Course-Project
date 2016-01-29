59f145d6eee6e21ba01fe9532e79840779519f19
Artem00ff <artem00gamov@gmail.com>
2016-01-29 12:54:58 +0300

    add run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,37 @@
+run_analysis <- function(outcome, num = "best") {
+  
+  
+  
+  
+  features <- read.table("./UCI HAR Dataset/features.txt", col.names = c("col_ID", "feature"), sep = "")
+  DataTest <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = features$feature)
+  DataTest_Y <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "labels")
+  
+  DataSubject_Test <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "Subject")
+  
+  DataTest$labels <- factor(DataTest_Y$labels, levels = c("1","2","3","4","5","6"), labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING")) 
+  DataTest$subject <- DataSubject_Test$Subject
+  
+  DataTrain <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features$feature)
+  DataTrain_Y <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "labels")
+  DataSubject_Train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "Subject")
+  
+  DataTrain$labels <- factor(DataTrain_Y$labels, levels = c("1","2","3","4","5","6"), labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+  DataTrain$subject <- DataSubject_Train$Subject
+  
+  Data <- rbind.data.frame(DataTest,DataTrain)
+  
+  y <- grep("*.mean*.|*.std*.", features$feature)
+  y <- c(y, 562, 563)
+  names(Data) <- gsub("\\.","",names(Data))
+  Data <- Data[,y]
+  
+  #ddply(Data[,y], .(labels,subject), summarize, mean=mean(names(Data[,y])))
+  
+  #Data5 <- ddply(Data[,y], .(labels,subject), summarize, mean=mean(noquote(names(Data[,y])),na.rm = TRUE)) 
+  
+  #Data5 <- ddply(Data[,y], .(labels,subject), summarize, mean=mean(tBodyAcc.mean...X, rm = TRUE),mean=mean(fBodyBodyGyroJerkMag.mean.., rm = TRUE))
+  #string <- "ddply(Data[,y], .(labels,subject), summarize"
+  #for (j in 1:79){string <- paste(string ,",", names(Data[,y])[j] , "=mean(" , names(Data[,y])[j] , ")" )} 
+  Data5 <- ddply(Data[,y], .(labels,subject), summarize , tBodyAccmeanX =mean( tBodyAccmeanX ) , tBodyAccmeanY =mean( tBodyAccmeanY ) , tBodyAccmeanZ =mean( tBodyAccmeanZ ) , tBodyAccstdX =mean( tBodyAccstdX ) , tBodyAccstdY =mean( tBodyAccstdY ) , tBodyAccstdZ =mean( tBodyAccstdZ ) , tGravityAccmeanX =mean( tGravityAccmeanX ) , tGravityAccmeanY =mean( tGravityAccmeanY ) , tGravityAccmeanZ =mean( tGravityAccmeanZ ) , tGravityAccstdX =mean( tGravityAccstdX ) , tGravityAccstdY =mean( tGravityAccstdY ) , tGravityAccstdZ =mean( tGravityAccstdZ ) , tBodyAccJerkmeanX =mean( tBodyAccJerkmeanX ) , tBodyAccJerkmeanY =mean( tBodyAccJerkmeanY ) , tBodyAccJerkmeanZ =mean( tBodyAccJerkmeanZ ) , tBodyAccJerkstdX =mean( tBodyAccJerkstdX ) , tBodyAccJerkstdY =mean( tBodyAccJerkstdY ) , tBodyAccJerkstdZ =mean( tBodyAccJerkstdZ ) , tBodyGyromeanX =mean( tBodyGyromeanX ) , tBodyGyromeanY =mean( tBodyGyromeanY ) , tBodyGyromeanZ =mean( tBodyGyromeanZ ) , tBodyGyrostdX =mean( tBodyGyrostdX ) , tBodyGyrostdY =mean( tBodyGyrostdY ) , tBodyGyrostdZ =mean( tBodyGyrostdZ ) , tBodyGyroJerkmeanX =mean( tBodyGyroJerkmeanX ) , tBodyGyroJerkmeanY =mean( tBodyGyroJerkmeanY ) , tBodyGyroJerkmeanZ =mean( tBodyGyroJerkmeanZ ) , tBodyGyroJerkstdX =mean( tBodyGyroJerkstdX ) , tBodyGyroJerkstdY =mean( tBodyGyroJerkstdY ) , tBodyGyroJerkstdZ =mean( tBodyGyroJerkstdZ ) , tBodyAccMagmean =mean( tBodyAccMagmean ) , tBodyAccMagstd =mean( tBodyAccMagstd ) , tGravityAccMagmean =mean( tGravityAccMagmean ) , tGravityAccMagstd =mean( tGravityAccMagstd ) , tBodyAccJerkMagmean =mean( tBodyAccJerkMagmean ) , tBodyAccJerkMagstd =mean( tBodyAccJerkMagstd ) , tBodyGyroMagmean =mean( tBodyGyroMagmean ) , tBodyGyroMagstd =mean( tBodyGyroMagstd ) , tBodyGyroJerkMagmean =mean( tBodyGyroJerkMagmean ) , tBodyGyroJerkMagstd =mean( tBodyGyroJerkMagstd ) , fBodyAccmeanX =mean( fBodyAccmeanX ) , fBodyAccmeanY =mean( fBodyAccmeanY ) , fBodyAccmeanZ =mean( fBodyAccmeanZ ) , fBodyAccstdX =mean( fBodyAccstdX ) , fBodyAccstdY =mean( fBodyAccstdY ) , fBodyAccstdZ =mean( fBodyAccstdZ ) , fBodyAccmeanFreqX =mean( fBodyAccmeanFreqX ) , fBodyAccmeanFreqY =mean( fBodyAccmeanFreqY ) , fBodyAccmeanFreqZ =mean( fBodyAccmeanFreqZ ) , fBodyAccJerkmeanX =mean( fBodyAccJerkmeanX ) , fBodyAccJerkmeanY =mean( fBodyAccJerkmeanY ) , fBodyAccJerkmeanZ =mean( fBodyAccJerkmeanZ ) , fBodyAccJerkstdX =mean( fBodyAccJerkstdX ) , fBodyAccJerkstdY =mean( fBodyAccJerkstdY ) , fBodyAccJerkstdZ =mean( fBodyAccJerkstdZ ) , fBodyAccJerkmeanFreqX =mean( fBodyAccJerkmeanFreqX ) , fBodyAccJerkmeanFreqY =mean( fBodyAccJerkmeanFreqY ) , fBodyAccJerkmeanFreqZ =mean( fBodyAccJerkmeanFreqZ ) , fBodyGyromeanX =mean( fBodyGyromeanX ) , fBodyGyromeanY =mean( fBodyGyromeanY ) , fBodyGyromeanZ =mean( fBodyGyromeanZ ) , fBodyGyrostdX =mean( fBodyGyrostdX ) , fBodyGyrostdY =mean( fBodyGyrostdY ) , fBodyGyrostdZ =mean( fBodyGyrostdZ ) , fBodyGyromeanFreqX =mean( fBodyGyromeanFreqX ) , fBodyGyromeanFreqY =mean( fBodyGyromeanFreqY ) , fBodyGyromeanFreqZ =mean( fBodyGyromeanFreqZ ) , fBodyAccMagmean =mean( fBodyAccMagmean ) , fBodyAccMagstd =mean( fBodyAccMagstd ) , fBodyAccMagmeanFreq =mean( fBodyAccMagmeanFreq ) , fBodyBodyAccJerkMagmean =mean( fBodyBodyAccJerkMagmean ) , fBodyBodyAccJerkMagstd =mean( fBodyBodyAccJerkMagstd ) , fBodyBodyAccJerkMagmeanFreq =mean( fBodyBodyAccJerkMagmeanFreq ) , fBodyBodyGyroMagmean =mean( fBodyBodyGyroMagmean ) , fBodyBodyGyroMagstd =mean( fBodyBodyGyroMagstd ) , fBodyBodyGyroMagmeanFreq =mean( fBodyBodyGyroMagmeanFreq ) , fBodyBodyGyroJerkMagmean =mean( fBodyBodyGyroJerkMagmean ) , fBodyBodyGyroJerkMagstd =mean( fBodyBodyGyroJerkMagstd ) , fBodyBodyGyroJerkMagmeanFreq =mean( fBodyBodyGyroJerkMagmeanFreq ))     
+}
